6c21eeb1c31aff5078944e00c69e3f8da854577a
Aidan Jones <aejones4gm@gmail.com>
2024-08-14 19:12:17 -0400

    Filler pages

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1>About Fallen Paradise TTRPG</h1>
+
+<p>This is a repository of information/rules for a TTRPG in a fantasy setting I have been working on for a while. The setting is intended to later be developed into a open world RPG video game.</p>
---
+++ src/routes/classes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Classes</h1>
+
+<p>This will eventually contain information about the different player classes in the TTRPG.</p>
