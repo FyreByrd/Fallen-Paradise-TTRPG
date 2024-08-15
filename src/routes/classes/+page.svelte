b69f3ec5b87376d1806374c877b339e24fed2e0e
Aidan Jones <aejones4gm@gmail.com>
2024-08-14 21:30:39 -0400

    Card lists

--- src/routes/classes/+page.svelte
+++ src/routes/classes/+page.svelte
@@ -7,5 +7,11 @@
 <p>This will eventually contain information about the different player classes in the TTRPG.</p>
 
 {#each data.classes as c}
-    <p>{c.meta.title}</p>
+    <div class="card bg-base-100 w-96 shadow-xl">
+        <div class="card-body">
+        <a href="{c.path}">
+            <h2 class="card-title">{c.meta.title}</h2>
+        </a>
+        </div>
+    </div>
 {/each}
