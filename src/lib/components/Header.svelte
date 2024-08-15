ec98bfdcd5cb56c078efa8766b5ff91d899c3dee
Aidan Jones <aejones4gm@gmail.com>
2024-08-15 14:55:54 -0400

    Move ancestries and classes to dropdown

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,10 +6,17 @@
         <div class="flex-none">
             <ul class="menu menu-horizontal px-1">
                 <li>
-                    <a href="/classes">Classes</a>
-                </li>
-                <li>
-                    <a href="/ancestries">Ancestries</a>
+                    <details>
+                        <summary>Character Creation</summary>
+                        <ul class="bg-base-200 rounded-t-none p-2">
+                            <li>
+                                <a href="/classes">Classes</a>
+                            </li>
+                            <li>
+                                <a href="/ancestries">Ancestries</a>
+                            </li>
+                        </ul>
+                    </details>
                 </li>
                 <li>
                     <a href="/about">About</a>
