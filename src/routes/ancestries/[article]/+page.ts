b095f2ff0c13abd6f91f382303f9925c9fa22487
Aidan Jones <aejones4gm@gmail.com>
2024-08-21 20:34:16 -0400

    Change slugs to be consistent

--- src/routes/ancestries/[article]/+page.ts
+++ src/routes/ancestries/[article]/+page.ts
@@ -2,5 +2,5 @@ import type { PageLoad } from './$types';
 import { postData } from '$lib/posts';
 
 export const load: PageLoad = async ({ params }) => {
-    return postData(await import(`../${params.other}.md`));
+    return postData(await import(`../${params.article}.md`));
 }
