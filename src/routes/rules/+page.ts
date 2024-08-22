2177be35e4d2d433137d8a591224e524b1af7e01
Aidan Jones <aejones4gm@gmail.com>
2024-08-21 21:35:41 -0400

    Fix error from new data

--- src/routes/rules/+page.ts
+++ src/routes/rules/+page.ts
@@ -3,7 +3,7 @@ import { postData, filterPosts } from '$lib/posts';
 
 export const load: PageLoad = async ({ params }) => {
 	return { 
-        members: await filterPosts(import.meta.glob(`./*.md`), "classes"),
+        members: await filterPosts(import.meta.glob(`./*.md`), "rules"),
         ...postData(await import(/* @vite-ignore */`./index.md`))
     };
 }
