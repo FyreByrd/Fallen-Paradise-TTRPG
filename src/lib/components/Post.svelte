ea1dcc0f291050b3b026049a43beae29fbcc7e25
Aidan Jones <aejones4gm@gmail.com>
2024-08-21 20:08:58 -0400

    Fixed date formatting

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -4,9 +4,15 @@
 
 <article>
     <h1>{data.title}</h1>
-    <p>Created: {data.created}<br>Last Updated: {data.modified}</p>
+    <p>
+        Created: {(new Date(data.created)).toLocaleDateString()}<br>
+        Updated: {(new Date(data.modified)).toLocaleString()}
+    </p>
     <hr>
     <svelte:component this={data.content} />
     <slot></slot>
-    <p>Last Updated: {data.modified}</p>
+    <hr>
+    <p>
+        Updated: {(new Date(data.modified)).toLocaleString()}
+    </p>
 </article>
