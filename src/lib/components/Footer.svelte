b978b31bdba37f33ef158fe45bf6fe8d187f27c2
Aidan Jones <aejones4gm@gmail.com>
2024-09-03 22:21:41 -0400

    D20 Favicon

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,6 @@
 <footer class="footer bg-neutral text-neutral-content p-10">
-    Copyright &copy; 2024 Aidan Jones
+    Copyright &copy; 2024 Aidan Jones<br>
+    <a href="https://www.flaticon.com/free-icons/d20" title="d20 icons">D20 icons created by Freepik - Flaticon</a>
 </footer>
 
 <style>
