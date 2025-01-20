eb127233e06946c3a3259ffc0e5b2d9eef25dc89
Aidan Jones <aejones4gm@gmail.com>
2025-01-19 22:26:15 -0500

    Removed mermaidjs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,42 +2,6 @@
     import "tailwindcss/tailwind.css";
     import Header from '$lib/components/Header.svelte';
     import Footer from "$lib/components/Footer.svelte";
-    import { afterUpdate } from "svelte";
-    import mermaid from "mermaid";
-
-    mermaid.initialize({ 
-        theme: 'neutral', 
-        startOnLoad: false 
-    });
-
-    afterUpdate(async () => {
-        const nodes = document.querySelectorAll("pre.language-mermaid");
-        nodes.forEach((pre) => {
-            pre.classList.replace("language-mermaid", "mermaid");
-            pre.querySelectorAll("code.language-mermaid").forEach((code) => {
-                code.querySelectorAll("span.token").forEach((s) => {
-                    s.childNodes.forEach((c) => {
-                        code.insertBefore(c, s);
-                    })
-                    code.removeChild(s);
-                });
-                pre.innerHTML = code.innerHTML;
-            })
-        });
-        if (nodes.length) {
-            try {
-                await mermaid.run();
-                nodes.forEach((n) => {
-                    Array.from(n.getElementsByTagName("svg")).forEach((svg) => {
-                        svg.removeAttribute("width");
-                    });
-                })
-            }
-            catch (e) {
-                console.log(e);
-            }
-        }
-    });
 </script>
 
 <Header />
