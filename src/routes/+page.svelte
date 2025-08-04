a77041fe49b4d6f37378cc4115f2225a20411265
Aidan Jones <aejones4gm@gmail.com>
2025-08-04 14:19:39 -0400

    Add link to commit history

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,2 @@
 <p>Welcome to Fallen Paradise TTRPG. This website is still under construction.</p>
+<p>View history on <a target="_blank" href="https://github.com/FyreByrd/Fallen-Paradise-TTRPG/commits/main">GitHub</a></p>
